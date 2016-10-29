5a29ef9afe2f0560f02e9f70a775635766cc1be7
Gabriele <unosviluppatore@users.noreply.github.com>
2016-10-29 14:52:28 +0200

    added comments from blog post

--- InitializerRewriter.cs
+++ InitializerRewriter.cs
@@ -19,29 +19,38 @@ namespace ConsoleApplication
 
         public override SyntaxNode VisitVariableDeclaration(VariableDeclarationSyntax node)
         {
+            // determination of the type of the variable(s)
             var typeSymbol = (ITypeSymbol)this.SemanticModel.GetSymbolInfo(node.Type).Symbol;            
             
             bool changed = false;
             
+            // you could declare more than one variable with one expression
             SeparatedSyntaxList<VariableDeclaratorSyntax> vs = node.Variables;
+            // we create a space to improve readability
             SyntaxTrivia space = SyntaxFactory.SyntaxTrivia(SyntaxKind.WhitespaceTrivia, " ");                       
 
             for (var i = 0; i < node.Variables.Count; i++)
             {
+                // there is not an initialization
                 if (this.SemanticModel.GetSymbolInfo(node.Type).Symbol.ToString() == "int" &&
                 node.Variables[i].Initializer == null)                            
                 {                                        
+                    // we create a new espression "42"
+                    // preceded by the space we create earlier
                     ExpressionSyntax es = SyntaxFactory.ParseExpression("42")
                                                        .WithLeadingTrivia(Space);                                        
-                    
+
+                    // basically we create an assignment to the espression we just created
                     EqualsValueClauseSyntax evc = SyntaxFactory.EqualsValueClause(es)
                                                                .WithLeadingTrivia(space);                
-                    
+
+                    // we replace the null initializer with ours
                     vs = vs.Replace(vs.ElementAt(i), vs.ElementAt(i).WithInitializer(evc));                                
 
                     changed = true;                 
                 }
-                
+
+                // there is an initialization but it's not to 42
                 if (this.SemanticModel.GetSymbolInfo(node.Type).Symbol.ToString() == "int" &&
                     node.Variables[i].Initializer != null &&
                     !node.Variables[i].Initializer.Value.IsEquivalentTo(SyntaxFactory.ParseExpression("42")))
--- Program.cs
+++ Program.cs
@@ -19,15 +19,19 @@ namespace ConsoleApplication
 
             foreach (SyntaxTree sourceTree in test.SyntaxTrees)
             {
+                // creation of the semantic model
                 SemanticModel model = test.GetSemanticModel(sourceTree);
 
+                // initialization of our rewriter class
                 InitializerRewriter rewriter = new InitializerRewriter(model);
 
+                // analysis of the tree
                 SyntaxNode newSource = rewriter.Visit(sourceTree.GetRoot());                
 
                 if(!Directory.Exists(@"../new_src"))
                     Directory.CreateDirectory(@"../new_src");
 
+                // if we changed the tree we save a new file
                 if (newSource != sourceTree.GetRoot())
                 {                    
                     File.WriteAllText(Path.Combine(@"../new_src", Path.GetFileName(sourceTree.FilePath)), newSource.ToFullString());
@@ -37,6 +41,7 @@ namespace ConsoleApplication
         
         private static Compilation CreateTestCompilation()
         {    
+            // creation of the syntax tree for every file
             String programPath = @"Program.cs";
             String programText = File.ReadAllText(programPath);
             SyntaxTree programTree = CSharpSyntaxTree.ParseText(programText)
@@ -49,12 +54,14 @@ namespace ConsoleApplication
 
             SyntaxTree[] sourceTrees = { programTree, rewriterTree };
 
+            // gathering the assemblies
             MetadataReference mscorlib =	MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location);
             MetadataReference codeAnalysis = MetadataReference.CreateFromFile(typeof(SyntaxTree).GetTypeInfo().Assembly.Location);
             MetadataReference csharpCodeAnalysis = MetadataReference.CreateFromFile(typeof(CSharpSyntaxTree).GetTypeInfo().Assembly.Location);                    
 
             MetadataReference[] references = { mscorlib, codeAnalysis, csharpCodeAnalysis };
 
+            // compilation
             return CSharpCompilation.Create( "ConsoleApplication",
 											 sourceTrees,
 											 references,
